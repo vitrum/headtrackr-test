b0ccaa305758f79b3db44d38dfde2310f160032c
vitrum.zhu <vitrum.cn@gmail.com>
2013-06-18 13:17:52 +0800

    update for mobile safari

--- ios7/js/ios7bg.js
+++ ios7/js/ios7bg.js
@@ -167,4 +167,48 @@ $(document).ready(function () {
 			phoneBgSwitch(0)
 		}
 	});
+	/* for mobile browser */
+
+
+	if (window.DeviceMotionEvent != undefined) {
+		var x = 0, y = 0,
+		    vx = 0, vy = 0,
+			ax = 0, ay = 0;
+		var bgBox = jQuery(".screen");
+
+		function boundingBoxCheck(){
+			if (x<0) { x = 0; vx = -vx; }
+			if (y<0) { y = 0; vy = -vy; }
+			if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
+			if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
+		}
+		window.ondevicemotion = function(e) {
+			ax = event.accelerationIncludingGravity.x * 5;
+			ay = event.accelerationIncludingGravity.y * 5;
+		
+		}
+		setInterval( function() {
+			var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
+
+			if ( landscapeOrientation) {
+				vx = vx + ay;
+				vy = vy + ax;
+			} else {
+				vy = vy - ay;
+				vx = vx + ax;
+			}
+			vx = vx * 0.98;
+			vy = vy * 0.98;
+			y = parseInt(y + vy / 50);
+			x = parseInt(x + vx / 50);
+			
+			boundingBoxCheck();
+			var newPositionX = -((Number(BgimgSize['width'])/2-160) - (Number(x) - 150))/2 ,
+			newPositionY =  -((Number(BgimgSize['height'])/2-220) - (Number(y) - 100))/2 ;
+			bgBox.css("background-position",newPositionX+"px "+ newPositionY+"px")
+			//sphere.style.top = y + "px";
+			//sphere.style.left = x + "px";
+			
+		}, 25);
+	}
 });
